ea092be404e8fc14662eb2c88206c3e0df8552c6
tordsyabe <extordsllave@gmail.com>
2020-01-22 03:55:46 +0800

    added grow on add boarder card

--- src/components/AddBoarder.js
+++ src/components/AddBoarder.js
@@ -18,7 +18,8 @@ import {
   Card,
   CardActions,
   CardContent,
-  Typography
+  Typography,
+  Grow
 } from '@material-ui/core';
 import { saveBoarder } from '../services/BoarderService';
 import { BoarderContext } from '../contexts/BoarderContext';
@@ -118,7 +119,7 @@ const AddBoarder = props => {
           )
         })
         .then(() => {
-          setDateJoined('');
+          setDateJoined(new Date());
 
           setHouseId('');
           setName('');
@@ -140,162 +141,164 @@ const AddBoarder = props => {
   };
 
   return (
-    <Card className={classes.card}>
-      <form onSubmit={handleSaveBoarder}>
-        <CardContent>
-          <Typography variant='h5'>Fill boarder information</Typography>
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <KeyboardDatePicker
-              disableToolbar
-              variant='inline'
-              format='MM/dd/yyyy'
-              margin='normal'
-              id='dateJoined'
-              label='Joining Date'
-              value={dateJoined}
-              onChange={handleDateChange}
-              KeyboardButtonProps={{
-                'aria-label': 'change date'
-              }}
+    <Grow in={true} timeout={500}>
+      <Card className={classes.card}>
+        <form onSubmit={handleSaveBoarder}>
+          <CardContent>
+            <Typography variant='h5'>Fill boarder information</Typography>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker
+                disableToolbar
+                variant='inline'
+                format='MM/dd/yyyy'
+                margin='normal'
+                id='dateJoined'
+                label='Joining Date'
+                value={dateJoined}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date'
+                }}
+                fullWidth
+                autoOk
+              />
+            </MuiPickersUtilsProvider>
+
+            <TextField
+              margin='dense'
+              id='name'
+              label='Boarder Name'
+              type='text'
               fullWidth
-              autoOk
+              variant='outlined'
+              required
+              value={name}
+              onChange={e => setName(e.target.value)}
             />
-          </MuiPickersUtilsProvider>
+            <Grid container>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  margin='dense'
+                  id='rootRate'
+                  label='Room Rate'
+                  type='number'
+                  variant='outlined'
+                  required
+                  value={roomRate}
+                  onChange={e => setRoomRate(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  margin='dense'
+                  id='utilities'
+                  label='Utilities'
+                  type='number'
+                  variant='outlined'
+                  required
+                  value={utilities}
+                  onChange={e => setUtilities(e.target.value)}
+                />
+              </Grid>
 
-          <TextField
-            margin='dense'
-            id='name'
-            label='Boarder Name'
-            type='text'
-            fullWidth
-            variant='outlined'
-            required
-            value={name}
-            onChange={e => setName(e.target.value)}
-          />
-          <Grid container>
-            <Grid item xs={12} md={6}>
-              <TextField
-                margin='dense'
-                id='rootRate'
-                label='Room Rate'
-                type='number'
-                variant='outlined'
-                required
-                value={roomRate}
-                onChange={e => setRoomRate(e.target.value)}
-              />
+              <Grid item xs={12} md={6}>
+                <TextField
+                  margin='dense'
+                  id='advancePayment'
+                  label='Advance Payment'
+                  type='number'
+                  variant='outlined'
+                  required
+                  value={advancePayment}
+                  onChange={e => setAdvancePayment(e.target.value)}
+                />
+              </Grid>
+
+              <Grid item xs={12} md={6}>
+                <TextField
+                  margin='dense'
+                  id='deposit'
+                  label='Deposit'
+                  type='number'
+                  variant='outlined'
+                  required
+                  value={deposit}
+                  onChange={e => setDeposit(e.target.value)}
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField
-                margin='dense'
-                id='utilities'
-                label='Utilities'
-                type='number'
-                variant='outlined'
-                required
-                value={utilities}
-                onChange={e => setUtilities(e.target.value)}
-              />
-            </Grid>
 
-            <Grid item xs={12} md={6}>
-              <TextField
-                margin='dense'
-                id='advancePayment'
-                label='Advance Payment'
-                type='number'
-                variant='outlined'
-                required
-                value={advancePayment}
-                onChange={e => setAdvancePayment(e.target.value)}
-              />
-            </Grid>
+            <TextField
+              select
+              fullWidth
+              variant='outlined'
+              margin='dense'
+              label='Select room number'
+              value={roomNumber}
+              required
+              onChange={e => {
+                setRoomNumber(e.target.value);
+              }}>
+              {rooms.map(room => (
+                <MenuItem key={room.number} value={room.number}>
+                  Room {room.number}
+                </MenuItem>
+              ))}
+            </TextField>
 
-            <Grid item xs={12} md={6}>
-              <TextField
-                margin='dense'
-                id='deposit'
-                label='Deposit'
-                type='number'
-                variant='outlined'
-                required
-                value={deposit}
-                onChange={e => setDeposit(e.target.value)}
-              />
-            </Grid>
-          </Grid>
+            <TextField
+              margin='dense'
+              id='typeOfRent'
+              label='Room Type'
+              type='text'
+              fullWidth
+              variant='outlined'
+              required
+              value={typeOfRent}
+              onChange={e => setTypeOfRent(e.target.value)}
+            />
 
-          <TextField
-            select
-            fullWidth
-            variant='outlined'
-            margin='dense'
-            label='Select room number'
-            value={roomNumber}
-            required
-            onChange={e => {
-              setRoomNumber(e.target.value);
-            }}>
-            {rooms.map(room => (
-              <MenuItem key={room.number} value={room.number}>
-                Room {room.number}
-              </MenuItem>
-            ))}
-          </TextField>
+            <TextField
+              margin='dense'
+              id='mobile'
+              label='Mobile Number'
+              type='text'
+              fullWidth
+              variant='outlined'
+              value={mobileNumber}
+              onChange={e => setMobileNumber(e.target.value)}
+            />
 
-          <TextField
-            margin='dense'
-            id='typeOfRent'
-            label='Room Type'
-            type='text'
-            fullWidth
-            variant='outlined'
-            required
-            value={typeOfRent}
-            onChange={e => setTypeOfRent(e.target.value)}
-          />
+            <TextField
+              margin='dense'
+              id='email'
+              label='Email'
+              type='email'
+              fullWidth
+              variant='outlined'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
 
-          <TextField
-            margin='dense'
-            id='mobile'
-            label='Mobile Number'
-            type='text'
-            fullWidth
-            variant='outlined'
-            value={mobileNumber}
-            onChange={e => setMobileNumber(e.target.value)}
-          />
-
-          <TextField
-            margin='dense'
-            id='email'
-            label='Email'
-            type='email'
-            fullWidth
-            variant='outlined'
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-
-          <TextField
-            margin='dense'
-            id='address'
-            label='Permanent Address'
-            type='text'
-            fullWidth
-            variant='outlined'
-            value={permanentAddress}
-            onChange={e => setPermanentAddress(e.target.value)}
-          />
-        </CardContent>
-        <CardActions>
-          <Button type='submit' color='primary' disabled={isSavingBoarder}>
-            Save
-          </Button>
-        </CardActions>
-      </form>
-    </Card>
+            <TextField
+              margin='dense'
+              id='address'
+              label='Permanent Address'
+              type='text'
+              fullWidth
+              variant='outlined'
+              value={permanentAddress}
+              onChange={e => setPermanentAddress(e.target.value)}
+            />
+          </CardContent>
+          <CardActions>
+            <Button type='submit' color='primary' disabled={isSavingBoarder}>
+              Save
+            </Button>
+          </CardActions>
+        </form>
+      </Card>
+    </Grow>
   );
 };
 
